2ef1c5dbd2190c20c23e1733cea225ddd36a3ae6
Alex Frutkin <afrutkin@pvschools.net>
2022-04-29 11:34:08 -0700

    moved create user function

--- src/LogIn.java
+++ src/LogIn.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class LogIn {
     // Records the current user
     private static int currentUser = -1;
@@ -8,6 +10,7 @@ public class LogIn {
             System.out.println("There are no users. Creating a user...");
             Data.addUser(true, password);
             System.out.println("User created successfully!");
+            System.out.println("Username: " + (Data.userList.size() - 1));
             currentUser = 0;
             return 0;
         }
@@ -19,6 +22,48 @@ public class LogIn {
             return 0;
         } else {
             return -1;
+        }
+    }
+
+    public static void createUser() {
+        Scanner scanner = new Scanner(System.in);
+        boolean notAdmin = true;
+        if (LogIn.getCurrentUser() != -1) {
+            if (Data.userList.get(LogIn.getCurrentUser()).isAdmin()) {
+                notAdmin = false;
+            }
+        }
+        while (notAdmin) {
+            System.out.println(
+                    "To do this you must log in as an admin. (if there is no admin account then type in 0 for username and set the password to the one you'd like to be associated with the first admin) -1 to cancel");
+            System.out.println("Enter your username: ");
+            int username = scanner.nextInt();
+            if (username == -1) {
+                break;
+            }
+            System.out.println("Enter your password: ");
+            String password = scanner.next();
+            int result = LogIn.logIn(username, password);
+            if (result == 0) {
+                System.out.println("Logged in successfully!");
+            } else {
+                System.out.println("Incorrect username or password.");
+            }
+            notAdmin = !Data.userList.get(LogIn.getCurrentUser()).isAdmin();
+        }
+        if (Data.userList.get(LogIn.getCurrentUser()).isAdmin()) {
+            System.out.println("Enter the password for the user you would like to create: ");
+            String password = scanner.next();
+            System.out.println("Is this user an admin? (y/n)");
+            String admin = scanner.next();
+            if (admin.equals("y")) {
+                Data.addUser(true, password);
+            } else {
+                Data.addUser(false, password);
+            }
+            System.out.println("User created successfully!");
+        } else {
+            System.out.println("You must log in as an admin to create a user.");
         }
     }
 
--- src/Runner.java
+++ src/Runner.java
@@ -23,44 +23,7 @@ public class Runner {
                 }
             } else if (input.equals("c")) {
                 // Create a new user if current user is an admin
-                boolean notAdmin = true;
-                if (LogIn.getCurrentUser() != -1) {
-                    if (Data.userList.get(LogIn.getCurrentUser()).isAdmin()) {
-                        notAdmin = false;
-                    }
-                }
-                while (notAdmin) {
-                    System.out.println(
-                            "To do this you must log in as an admin. (if there is no admin account then type in 0 for username and set the password to the one you'd like to be associated with the first admin) -1 to cancel");
-                    System.out.println("Enter your username: ");
-                    int username = scanner.nextInt();
-                    if (username == -1) {
-                        break;
-                    }
-                    System.out.println("Enter your password: ");
-                    String password = scanner.next();
-                    int result = LogIn.logIn(username, password);
-                    if (result == 0) {
-                        System.out.println("Logged in successfully!");
-                    } else {
-                        System.out.println("Incorrect username or password.");
-                    }
-                    notAdmin = !Data.userList.get(LogIn.getCurrentUser()).isAdmin();
-                }
-                if (Data.userList.get(LogIn.getCurrentUser()).isAdmin()) {
-                    System.out.println("Enter the password for the user you would like to create: ");
-                    String password = scanner.next();
-                    System.out.println("Is this user an admin? (y/n)");
-                    String admin = scanner.next();
-                    if (admin.equals("y")) {
-                        Data.addUser(true, password);
-                    } else {
-                        Data.addUser(false, password);
-                    }
-                    System.out.println("User created successfully!");
-                } else {
-                    System.out.println("You must log in as an admin to create a user.");
-                }
+                LogIn.createUser();
             } else if (input.equals("q")) {
                 // Exit the program
                 running = false;
