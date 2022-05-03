77833d148e7d5541376efcdd27a50bef987d29d3
hblackbehr <hblackbehr@gmail.com>
2022-05-03 12:03:35 -0700

    find total time worked

--- src/Clock.java
+++ src/Clock.java
@@ -3,8 +3,7 @@ import java.util.Calendar;
 // Manage time functions
 public class Clock {
     // clock in
-    public double timeIn;
-    public double timeOut;
+    public static double timeOut;
 
     public static void clockIn() {
         Calendar calendar = Calendar.getInstance();
@@ -14,7 +13,7 @@ public class Clock {
             // clock in
             Data.userList.get(LogIn.getCurrentUser()).setClockedIn(true);
             // set the clock in time
-            timeIn = Data.userList.get(LogIn.getCurrentUser()).setClockInTime(System.currentTimeMillis());
+            Data.userList.get(LogIn.getCurrentUser()).setClockInTime(System.currentTimeMillis());
             //Data.userList.get(LogIn.getCurrentUser()).setClockInTime(System.currentTimeMillis());
             // print that the user has clocked in
             System.out.println("You have clocked in.");
@@ -32,7 +31,7 @@ public class Clock {
             // clock out
             Data.userList.get(LogIn.getCurrentUser()).setClockedIn(false);
             // record time worked to the correct day of the week in timeList
-            timeOut = Data.userList.get(LogIn.getCurrentUser()).setClockOutTime(System.currentTimeMillis());
+            Data.userList.get(LogIn.getCurrentUser()).setClockOutTime(System.currentTimeMillis());
             //Data.userList.get(LogIn.getCurrentUser()).setClockOutTime(System.currentTimeMillis());
             // print that the user has clocked out
             System.out.println("You have clocked out.");
@@ -44,7 +43,7 @@ public class Clock {
 
     public static double totalTime(double a) {
         //end time subtracted from start time
-        double total = timeOut - timeIn;
+        double total = System.currentTimeMillis() - Data.userList.get(LogIn.getCurrentUser()).getClockInTime();
         return total;
         //return 0.0;
     }
