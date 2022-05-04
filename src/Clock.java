a764cd5c6a4592648b79907b86bc9cbfdbb0070b
Alex Frutkin <afrutkin@pvschools.net>
2022-05-04 10:46:19 -0700

    recording time should now work

--- src/Clock.java
+++ src/Clock.java
@@ -6,15 +6,13 @@ public class Clock {
     public static double timeOut;
 
     public static void clockIn() {
-        Calendar calendar = Calendar.getInstance();
-        System.out.println(calendar.get(Calendar.DAY_OF_WEEK));
         // if the user is not clocked in
         if (!Data.userList.get(LogIn.getCurrentUser()).isClockedIn()) {
             // clock in
             Data.userList.get(LogIn.getCurrentUser()).setClockedIn(true);
             // set the clock in time
             Data.userList.get(LogIn.getCurrentUser()).setClockInTime(System.currentTimeMillis());
-            //Data.userList.get(LogIn.getCurrentUser()).setClockInTime(System.currentTimeMillis());
+            // Data.userList.get(LogIn.getCurrentUser()).setClockInTime(System.currentTimeMillis());
             // print that the user has clocked in
             System.out.println("You have clocked in.");
         } else {
@@ -26,13 +24,13 @@ public class Clock {
 
     // clock out
     public static void clockOut() {
+        Calendar calendar = Calendar.getInstance();
         // if the user is clocked in
         if (Data.userList.get(LogIn.getCurrentUser()).isClockedIn()) {
             // clock out
             Data.userList.get(LogIn.getCurrentUser()).setClockedIn(false);
             // record time worked to the correct day of the week in timeList
-            //Data.userList.get(LogIn.getCurrentUser()).setClockOutTime(System.currentTimeMillis());
-            //Data.userList.get(LogIn.getCurrentUser()).setClockOutTime(System.currentTimeMillis());
+            Data.recordTime(LogIn.getCurrentUser(), calendar.get(Calendar.DAY_OF_WEEK), totalTime());
             // print that the user has clocked out
             System.out.println("You have clocked out.");
         } else {
@@ -41,10 +39,10 @@ public class Clock {
         }
     }
 
-    public static double totalTime(double a) {
-        //end time subtracted from start time
-        double total = System.currentTimeMillis() - Data.userList.get(LogIn.getCurrentUser()).getClockInTime();
+    private static long totalTime() {
+        // end time subtracted from start time
+        long total = System.currentTimeMillis() - Data.userList.get(LogIn.getCurrentUser()).getClockInTime();
         return total;
-        //return 0.0;
+        // return 0.0;
     }
 }
