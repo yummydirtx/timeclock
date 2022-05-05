dabf8e571008dee47b8a6b584663bcd6c242353e
Alex Frutkin <afrutkin@pvschools.net>
2022-05-05 10:52:43 -0700

    reordered the creation for logic's sake

--- src/Runner.java
+++ src/Runner.java
@@ -7,15 +7,12 @@ public class Runner {
         boolean running = true;
         while (running) {
             // Ask if the user would like to login
-            System.out.println("Would you like to login, or create a new user? (l) or (c) (q to exit)");
+            System.out.println("Would you like to login? (l) (q to exit)");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             if (input.equals("l")) {
                 // Run the login
                 LogIn.loginPrompt();
-            } else if (input.equals("c")) {
-                // Create a new user if current user is an admin
-                LogIn.createUser();
             } else if (input.equals("q")) {
                 // Exit the program
                 running = false;
@@ -28,7 +25,7 @@ public class Runner {
                 while (loggedIn) {
                     // If the user is logged in, ask if they would like to do something else
                     System.out.println(
-                            "What would you like to do? Clock (i)n, Clock (o)ut, (a)dmin console, or (l)ogout");
+                            "What would you like to do? Clock (i)n, Clock (o)ut, (a)dmin console, (c)reate a user, or (l)ogout");
                     input = scanner.nextLine();
                     if (input.equals("i")) {
                         // Clock in
@@ -43,6 +40,9 @@ public class Runner {
                         // Logout
                         LogIn.logOut();
                         loggedIn = false;
+                    } else if (input.equals("c")) {
+                        // Create a new user
+                        LogIn.createUser();
                     } else {
                         // Invalid input
                         System.out.println("Invalid input.");
