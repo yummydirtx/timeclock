ddac2511c42053254cf835ddf782d45a731645d3
Alex Frutkin <afrutkin@pvschools.net>
2022-05-05 11:47:57 -0700

    added header and fun opener

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,25 @@
+/*
+Copyright (c) 2022 Alex Frutkin and Hannah Behrend
+
+This file is part of TimeClock.
+
+TimeClock is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+TimeClock is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) throws Exception {
+        System.out.println(" _______ _____ _______ _______ _______         _____  _______ _     _\n" +
+                "|      |   |  |  | |______ |       |      |     | |       |____/\n" +
+                "    |    __|__ |  |  | |______ |_____  |_____ |_____| |_____  |    \\_\n" +
+                "Created by Alex Frutkin and Hannah Behrend\n" +
+                "Version 1.0 (5/5/2022) Licensed under GNU GPLv3+\n" +
+                "Source code available at: https://github.com/yummydirtx/timeclock");
         // Control the main logic loop
         Data.loadData();
         boolean running = true;
